1b6b637257e7a02add50ff36aa684d78a3a69208
Sheena Bonilla <sheena.myb@gmail.com>
2020-06-10 03:50:47 +0000

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,25 @@
 class Admin::PreferencesController < ApplicationController
 
   def index
+    @preferences = Preference.all
+  end
+
+  def new
+    @preference = Preference.new
+  end
+
+  def create
+    @preference = Preference.new(preference_params)
+
+    if @preference.save
+      redirect_to @preference
+    else
+      render :new
+    end
+  end
+
+  def preference_params
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists)
   end
 
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,3 @@
 class Preference < ActiveRecord::Base
 
-
 end
